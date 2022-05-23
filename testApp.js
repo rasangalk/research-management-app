8017aa66cfa80f179517ac58306a6718dc06c56c
Nimesha Muthukumarana <nimesha.muthukumarana@gmail.com>
2022-05-24 02:19:47 +0530

    Members Student Designed

--- testApp.js
+++ testApp.js
@@ -1,50 +1,56 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { isUserLoggedIn } from "./src/actions/auth.actions";
-import PrivateWrapper from "./src/components/HOC/PrivateRoute";
-import Signin from "./src/containers/Signin";
-import Signup from "./src/containers/Register";
-import LandingPage from "./src/containers/LandingPage";
-import StudentRegister from "./src/containers/Student/StudentRegister";
-import StudentHome from "./src/containers/Student/StudentHome/Home";
-import StudentSubmission from "./src/containers/Student/StudentSubmission/StudentSubmission";
-import StudentFeedbackAccept from "./src/containers/Student/FeedbackAccepted/FeedbackAccepted";
-import TopicRegistration from "./src/containers/Student/StudentTopicRegister/TopicRegistration";
-import AdminHome from "./src/containers/Admin/Members/Members";
+import React, { useEffect } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { isUserLoggedIn } from './src/actions/auth.actions'
+import PrivateWrapper from './src/components/HOC/PrivateRoute'
+import Signin from './src/containers/Signin'
+import Signup from './src/containers/Register'
+import LandingPage from './src/containers/LandingPage'
+import StudentRegister from './src/containers/Student/StudentRegister'
+import StudentHome from './src/containers/Student/StudentHome/Home'
+import StudentSubmission from './src/containers/Student/StudentSubmission/StudentSubmission'
+import StudentFeedbackAccept from './src/containers/Student/FeedbackAccepted/FeedbackAccepted'
+import TopicRegistration from './src/containers/Student/StudentTopicRegister/TopicRegistration'
+import AdminHome from './src/containers/Admin/Members/Members'
+import MembersStudent from './src/containers/Admin/Members-Student/MembersStudent'
 
 function App() {
-	const dispatch = useDispatch();
-	const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch()
+  const auth = useSelector((state) => state.auth)
 
-	useEffect(() => {
-		if (!auth.authenticate) {
-			dispatch(isUserLoggedIn());
-		}
-	}, [auth.authenticate]);
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn())
+    }
+  }, [auth.authenticate])
 
-	return (
-		<div className='App'>
-			<Routes>
-				<Route path='/submission' element={<StudentSubmission />} />
-				<Route path='/signin' element={<Signin />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/landing-page' element={<LandingPage />} />
-				<Route path='/student/signup' element={<StudentRegister />} />
-				<Route path='/student-home' exact element={<PrivateWrapper />}>
-					<Route path='/student-home' element={<StudentHome />} />
-				</Route>
-				<Route path='/r2' element={<StudentFeedbackAccept />} />
-				<Route
-					path='/student/topic-registration'
-					element={<TopicRegistration />}
-				/>
-				<Route path='/r2' element={<StudentFeedbackAccept />} />
-				<Route path='/r3' element={<AdminHome />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/submission' element={<StudentSubmission />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/landing-page' element={<LandingPage />} />
+        <Route path='/student/signup' element={<StudentRegister />} />
+        <Route path='/student-home' exact element={<PrivateWrapper />}>
+          <Route path='/student-home' element={<StudentHome />} />
+        </Route>
+        <Route path='/r2' element={<StudentFeedbackAccept />} />
+        <Route
+          path='/student/topic-registration'
+          element={<TopicRegistration />}
+        />
+        <Route path='/r2' element={<StudentFeedbackAccept />} />
+        <Route path='/r3' element={<AdminHome />} />
+
+        <Route
+          path='/Members-Student/MembersStudent'
+          element={<MembersStudent />}
+        />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
