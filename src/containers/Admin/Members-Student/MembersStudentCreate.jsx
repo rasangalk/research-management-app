562f86b2dfeee9be76f17c654461b37bd6a2cfd9
Nimesha Muthukumarana <nimesha.muthukumarana@gmail.com>
2022-05-25 01:39:25 +0530

    Members Student Create Designed

--- src/containers/Admin/Members-Student/MembersStudentCreate.jsx
+++ src/containers/Admin/Members-Student/MembersStudentCreate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Menu from '../../../components/AdminMenu'
 import Container from '../../../components/StudentContainer'
 
-function MembersStudentUpdate() {
+function MembersStudentCreate() {
   return (
     <div>
       <div className='w-screen h-screen grid grid-cols-12 bg-bk-white'>
@@ -75,6 +75,9 @@ function MembersStudentUpdate() {
                       name='Specailization'
                       className=' font-Nunito font-semibold  bg-white border border-slate-300 focus:outline-none focus:border-gray-500 focus:ring-gray-500 block lg:w-[25rem] sm:w-[25rem] rounded-md text-base focus:ring-1 px-3 py-2'
                     >
+                      <option value='' disabled selected hidden>
+                        Select Your Specailization
+                      </option>
                       <option value='bla'>Software Engineering</option>
                       <option value='blaa'>blaa</option>
                       <option value='blaaa'>blaaa</option>
@@ -84,12 +87,9 @@ function MembersStudentUpdate() {
                   </div>
                 </div>
               </div>
-              <div className='flex mt-[5.3rem] gap-3 ml-[51.5rem] '>
-                <button className='  text-lg  px-6 py-1 bg-[#F65354] text-white border border-slate-300 focus:outline-none focus:border-[#f76d6d]-500 focus:ring-sky-500 block lg:w-50 sm:w-50 rounded-md hover:bg-[#c73e3e]-active float-right  text-sm'>
-                  Cancel
-                </button>
+              <div className='mt-[5.3rem]  ml-[58.3rem] '>
                 <button className=' text-lg  px-6 py-1 bg-regal-blue text-white border border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block lg:w-50 sm:w-50 rounded-md hover:bg-regal-blue-active float-right  text-sm'>
-                  Update
+                  Create
                 </button>
               </div>
             </div>
@@ -100,4 +100,4 @@ function MembersStudentUpdate() {
   )
 }
 
-export default MembersStudentUpdate
+export default MembersStudentCreate
