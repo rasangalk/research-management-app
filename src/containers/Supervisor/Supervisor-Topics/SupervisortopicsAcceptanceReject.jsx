b2fc7b46f3c0f9b8e05c2d1ad66901282d625bdd
Nimesha Muthukumarana <nimesha.muthukumarana@gmail.com>
2022-05-26 22:39:29 +0530

    Supervisor Topics Acceptance/Reject Designed

---
+++ src/containers/Supervisor/Supervisor-Topics/SupervisortopicsAcceptanceReject.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Menu from '../../../components/SupervisorMenu'
+import Container from '../../../components/SupervisorContainer'
+
+function SupervisortopicsAcceptanceReject() {
+  return (
+    <div>
+      <div className='w-screen h-screen grid grid-cols-12 bg-bk-white'>
+        <div className=''>
+          <Menu />
+        </div>
+        <div className='col-span-11 mx-auto my-auto '>
+          <div className='relative'>
+            <Container title='Profile' width='w-[68rem] h-[24.9rem]' />
+            <div className='absolute top-[10rem] left-[7rem]'>
+              <div className=' grid grid-cols-2 w-[60rem]'>
+                <div className='flex flex-col gap-8'>
+                  <div>
+                    <p className='capitalize text-slate-700 text-sm mb-1'>
+                      Group ID
+                    </p>
+                    <p className=' font-bold text-base mb-2'>RE_GR24</p>
+                  </div>
+                  <div>
+                    <p className='capitalize text-slate-700 text-sm mb-1'>
+                      Topic
+                    <p className='capitalize font-bold text-base mb-8'>
+                      Blockchain based smart contracts and its applications
+                    </p>
+                  </div>
+                  <div>
+                    <p className='capitalize text-base text-slate-700 text-sm mb-1'>
+                      Comment
+                    </p>
+                    <input
+                      type='text'
+                      name='Comment'
+                      className='font-Nunito font-semibold  bg-white border border-slate-300 focus:outline-none focus:border-gray-500 focus:ring-gray-500 block lg:w-[60rem] sm:w-[60rem] rounded-md text-base focus:ring-1 px-3 py-6 '
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col gap-8'>
+                  <div>
+                    <p className='capitalize text-slate-700 text-sm mb-1'>
+                      Interest
+                    </p>
+                    <p className='capitalize font-bold text-base'>
+                      Distributed Systems
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className='flex mt-[4.8rem] gap-3 ml-[51.8rem] '>
+                <button className='  text-lg  px-6 py-1 bg-[#F65354] text-white border border-slate-300 focus:outline-none focus:border-[#f76d6d]-500 focus:ring-sky-500 block lg:w-50 sm:w-50 rounded-md hover:bg-[#c73e3e]-active text-sm'>
+                  Reject
+                </button>
+                <button className=' text-lg  px-6 py-1 bg-regal-blue text-white border border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block lg:w-50 sm:w-50 rounded-md hover:bg-regal-blue-active text-sm'>
+                  Accept
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SupervisortopicsAcceptanceReject
--- testApp.js
+++ testApp.js
@@ -1,134 +1,141 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { isUserLoggedIn } from "./src/actions/auth.actions";
-import PrivateWrapper from "./src/components/HOC/PrivateRoute";
-import Signin from "./src/containers/Signin";
-import Signup from "./src/containers/Register";
-import LandingPage from "./src/containers/LandingPage";
-import StudentRegister from "./src/containers/Student/StudentRegister";
-import StudentHome from "./src/containers/Student/StudentHome/Home";
-import StudentSubmission from "./src/containers/Student/StudentSubmission/StudentSubmission";
-import StudentFeedbackAccept from "./src/containers/Student/FeedbackAccepted/FeedbackAccepted";
-import StudentTemplateList from "./src/containers/Student/StudentTemplateList";
-import StudentFileSubmission from "./src/containers/Student/StudentFileSubmission/StudentFileSubmission";
-import AdminHome from "./src/containers/Admin/Members/Members";
-import MemberDetails from "./src/containers/Admin/Members-Student/MembersStudent";
-import CreateSubmission from "./src/containers/Admin/CreateSubmission/CreateSubmission";
-import ViewSubmission from "./src/containers/Admin/ViewSubmission/ViewSubmission";
-import MembersStudentUpdate from "./src/containers/Admin/Members-Student/MembersStudentUpdate";
-import MembersCreate from "./src/containers/Admin/Members-Student/MembersStudentCreate";
-import TopicRegistration from "./src/containers/Student/StudentTopicRegister/TopicRegistration";
-import SupervisorProfile from "./src/containers/Supervisor/Supervisor-Profile/SupervisorProfile";
-import Panels from "./src/containers/Admin/Panels/Panels";
-import Groups from "./src/containers/Admin/Groups/Groups";
-import Templates from "./src/containers/Admin/Templates/Templates";
-import CreatePannel from "./src/containers/Admin/Panels/CreatePannel";
-import EditSubmission from "./src/containers/Admin/EditSubmission/EditSubmission";
-import GroupManipulations from "./src/containers/Admin/Groups/GroupManipulations";
-import PannelView from "./src/containers/Admin/Panels/PannelView";
+import React, { useEffect } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { isUserLoggedIn } from './src/actions/auth.actions'
+import PrivateWrapper from './src/components/HOC/PrivateRoute'
+import Signin from './src/containers/Signin'
+import Signup from './src/containers/Register'
+import LandingPage from './src/containers/LandingPage'
+import StudentRegister from './src/containers/Student/StudentRegister'
+import StudentHome from './src/containers/Student/StudentHome/Home'
+import StudentSubmission from './src/containers/Student/StudentSubmission/StudentSubmission'
+import StudentFeedbackAccept from './src/containers/Student/FeedbackAccepted/FeedbackAccepted'
+import StudentTemplateList from './src/containers/Student/StudentTemplateList'
+import StudentFileSubmission from './src/containers/Student/StudentFileSubmission/StudentFileSubmission'
+import AdminHome from './src/containers/Admin/Members/Members'
+import MemberDetails from './src/containers/Admin/Members-Student/MembersStudent'
+import CreateSubmission from './src/containers/Admin/CreateSubmission/CreateSubmission'
+import ViewSubmission from './src/containers/Admin/ViewSubmission/ViewSubmission'
+import MembersStudentUpdate from './src/containers/Admin/Members-Student/MembersStudentUpdate'
+import MembersCreate from './src/containers/Admin/Members-Student/MembersStudentCreate'
+import TopicRegistration from './src/containers/Student/StudentTopicRegister/TopicRegistration'
+import SupervisorProfile from './src/containers/Supervisor/Supervisor-Profile/SupervisorProfile'
+import Panels from './src/containers/Admin/Panels/Panels'
+import Groups from './src/containers/Admin/Groups/Groups'
+import Templates from './src/containers/Admin/Templates/Templates'
+import CreatePannel from './src/containers/Admin/Panels/CreatePannel'
+import EditSubmission from './src/containers/Admin/EditSubmission/EditSubmission'
+import GroupManipulations from './src/containers/Admin/Groups/GroupManipulations'
+import PannelView from './src/containers/Admin/Panels/PannelView'
+import SupervisortopicsAcceptanceReject from './src/containers/Supervisor/Supervisor-Topics/SupervisortopicsAcceptanceReject'
 
 function App() {
-	const dispatch = useDispatch();
-	const auth = useSelector(state => state.auth);
+  const dispatch = useDispatch()
+  const auth = useSelector((state) => state.auth)
 
-	useEffect(() => {
-		if (!auth.authenticate) {
-			dispatch(isUserLoggedIn());
-		}
-	}, [auth.authenticate]);
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn())
+    }
+  }, [auth.authenticate])
 
-	return (
-		<div className='App'>
-			<Routes>
-				////////////////////Common/////////////////////////////
-				<Route path='/' element={<Signin />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/landing-page' element={<LandingPage />} />
-				/////////////////////Student//////////////////////////
-				<Route path='/student/signup' element={<StudentRegister />} />
-				<Route path='/student/submission' element={<PrivateWrapper />}>
-					<Route path='/student/submission' element={<StudentSubmission />} />
-				</Route>
-				<Route path='/student-home' element={<PrivateWrapper />}>
-					<Route path='/student-home' element={<StudentHome />} />
-				</Route>
-				<Route path='/r2' element={<PrivateWrapper />}>
-					<Route path='/r2' element={<StudentFeedbackAccept />} />
-				</Route>
-				<Route path='/student/templates' element={<PrivateWrapper />}>
-					<Route path='/student/templates' element={<StudentTemplateList />} />
-				</Route>
-				<Route path='/student/file-submission' element={<PrivateWrapper />}>
-					<Route
-						path='/student/file-submission'
-						element={<StudentFileSubmission />}
-					/>
-				</Route>
-				<Route path='/student/topicRegistration' element={<PrivateWrapper />}>
-					<Route
-						path='/student/topicRegistration'
-						element={<TopicRegistration />}
-					/>
-				</Route>
-				///////////////////////Admin///////////////////////////////
-				<Route path='/admin/members' element={<PrivateWrapper />}>
-					<Route path='/admin/members' element={<AdminHome />} />
-				</Route>
-				<Route path='/admin/submissions/create' element={<PrivateWrapper />}>
-					<Route
-						path='/admin/submissions/create'
-						element={<CreateSubmission />}
-					/>
-				</Route>
-				<Route path='/admin/submissions/view' element={<PrivateWrapper />}>
-					<Route path='/admin/submissions/view' element={<ViewSubmission />} />
-				</Route>
-				<Route path='/admin/members/:memberId' element={<PrivateWrapper />}>
-					<Route path='/admin/members/:memberId' element={<MemberDetails />} />
-				</Route>
-				<Route
-					path='/Members-Student/MemberStudentUpdate'
-					element={<PrivateWrapper />}>
-					<Route
-						path='/Members-Student/MemberStudentUpdate'
-						element={<MembersStudentUpdate />}
-					/>
-				</Route>
-				<Route path='/admin/member/create' element={<PrivateWrapper />}>
-					<Route path='/admin/member/create' element={<MembersCreate />} />
-				</Route>
-				<Route path='/admin/view-groups' element={<PrivateWrapper />}>
-					<Route path='/admin/view-groups' element={<Groups />} />
-				</Route>
-				<Route path='/admin/create-panel' element={<PrivateWrapper />}>
-					<Route path='/admin/create-panel' element={<CreatePannel />} />
-				</Route>
-				<Route path='/admin/panels' element={<PrivateWrapper />}>
-					<Route path='/admin/panels' element={<Panels />} />
-				</Route>
-				<Route path='/admin/submissions/edit' element={<PrivateWrapper />}>
-					<Route path='/admin/submissions/edit' element={<EditSubmission />} />
-				</Route>
-				<Route path='/r8' element={<PrivateWrapper />}>
-					<Route path='/r8' element={<GroupManipulations />} />
-				</Route>
-				<Route path='/r9' element={<PrivateWrapper />}>
-					<Route path='/r9' element={<PannelView />} />
-				</Route>
-				//////////////////////////////Supervisor////////////////////////
-				<Route
-					path='/Supervisor/Supervisor-Profile'
-					element={<PrivateWrapper />}>
-					<Route
-						path='/Supervisor/Supervisor-Profile'
-						element={<SupervisorProfile />}
-					/>
-				</Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Routes>
+        ////////////////////Common/////////////////////////////
+        <Route path='/' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/landing-page' element={<LandingPage />} />
+        /////////////////////Student//////////////////////////
+        <Route path='/student/signup' element={<StudentRegister />} />
+        <Route path='/student/submission' element={<PrivateWrapper />}>
+          <Route path='/student/submission' element={<StudentSubmission />} />
+        </Route>
+        <Route path='/student-home' element={<PrivateWrapper />}>
+          <Route path='/student-home' element={<StudentHome />} />
+        </Route>
+        <Route path='/r2' element={<PrivateWrapper />}>
+          <Route path='/r2' element={<StudentFeedbackAccept />} />
+        </Route>
+        <Route path='/student/templates' element={<PrivateWrapper />}>
+          <Route path='/student/templates' element={<StudentTemplateList />} />
+        </Route>
+        <Route path='/student/file-submission' element={<PrivateWrapper />}>
+          <Route
+            path='/student/file-submission'
+            element={<StudentFileSubmission />}
+          />
+        </Route>
+        <Route path='/student/topicRegistration' element={<PrivateWrapper />}>
+          <Route
+            path='/student/topicRegistration'
+            element={<TopicRegistration />}
+          />
+        </Route>
+        ///////////////////////Admin///////////////////////////////
+        <Route path='/admin/members' element={<PrivateWrapper />}>
+          <Route path='/admin/members' element={<AdminHome />} />
+        </Route>
+        <Route path='/admin/submissions/create' element={<PrivateWrapper />}>
+          <Route
+            path='/admin/submissions/create'
+            element={<CreateSubmission />}
+          />
+        </Route>
+        <Route path='/admin/submissions/view' element={<PrivateWrapper />}>
+          <Route path='/admin/submissions/view' element={<ViewSubmission />} />
+        </Route>
+        <Route path='/admin/members/:memberId' element={<PrivateWrapper />}>
+          <Route path='/admin/members/:memberId' element={<MemberDetails />} />
+        </Route>
+        <Route
+          path='/Members-Student/MemberStudentUpdate'
+          element={<PrivateWrapper />}
+        >
+          <Route
+            path='/Members-Student/MemberStudentUpdate'
+            element={<MembersStudentUpdate />}
+          />
+        </Route>
+        <Route path='/admin/member/create' element={<PrivateWrapper />}>
+          <Route path='/admin/member/create' element={<MembersCreate />} />
+        </Route>
+        <Route path='/admin/view-groups' element={<PrivateWrapper />}>
+          <Route path='/admin/view-groups' element={<Groups />} />
+        </Route>
+        <Route path='/admin/create-panel' element={<PrivateWrapper />}>
+          <Route path='/admin/create-panel' element={<CreatePannel />} />
+        </Route>
+        <Route path='/admin/panels' element={<PrivateWrapper />}>
+          <Route path='/admin/panels' element={<Panels />} />
+        </Route>
+        <Route path='/admin/submissions/edit' element={<PrivateWrapper />}>
+          <Route path='/admin/submissions/edit' element={<EditSubmission />} />
+        </Route>
+        <Route path='/r8' element={<PrivateWrapper />}>
+          <Route path='/r8' element={<GroupManipulations />} />
+        </Route>
+        <Route path='/r9' element={<PrivateWrapper />}>
+          <Route path='/r9' element={<PannelView />} />
+        </Route>
+        //////////////////////////////Supervisor////////////////////////
+        <Route
+          path='/Supervisor/Supervisor-Profile'
+          element={<PrivateWrapper />}
+        >
+          <Route
+            path='/Supervisor/Supervisor-Profile'
+            element={<SupervisorProfile />}
+          />
+        </Route>
+        <Route
+          path='/Supervisor/Supervisor-Topics-AcceptanceReject'
+          element={<SupervisortopicsAcceptanceReject />}
+        />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
